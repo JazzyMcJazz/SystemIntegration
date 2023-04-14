4b116cd896e3be111de0d712360e89e6e68ef626
Lars Riel <larsriel@gmail.com>
2023-04-14 16:07:44 +0200

    homework

--- 16_CORS/node/app.js
+++ 16_CORS/node/app.js
@@ -4,11 +4,19 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/timestamp', (req, res) => {
     res.send(new Date());
 });
 
+app.post('/webhook', (req, res) => {
+    console.log('Webhook called');
+    console.log(req.body);
+    res.send();
+});
+
+
 app.listen(3002, () => {
-    console.log('Server started on port 3000');
+    console.log('Server started on port 3002');
 });
